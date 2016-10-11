57357a26c70b4a27f044198d4059eabf57e9553e
残月 <632342527@qq.com>
2016-10-11 08:52:16 +0800

    Initial Commit

---
+++ ADFileManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADFileManager
+//
+//  Created by 王奥东 on 16/10/11.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADFileManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADFileManager
+//
+//  Created by 王奥东 on 16/10/11.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
